2723a174afaa1025dfd9e2ec2fcc7e9af1332726
Tran Phuoc Hai <haitran080425314@gmail.com>
2026-01-22 18:08:47 +0700

    updat exam page

--- src/apps/web/app/exam/page.tsx
+++ src/apps/web/app/exam/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import ExamList from '@/components/exam/ExamList';
 import DashboardLayout from '@/components/layout/DashboardLayout';
 
@@ -10,30 +10,30 @@ export default function ExamTab() {
       <div className="exam-container fade-in flex flex-col relative min-h-full">
         
         {/* 1. Header Banner */}
-        {/* THAY ĐỔI QUAN TRỌNG: 
-            - Thêm 'card-blue': Để nhận nền xanh và chữ trắng từ globals.css (ghi đè lên màu trắng của .card).
-            - Giữ 'card' & 'group': Để có bo góc, bóng đổ và hiệu ứng hover zoom.
-        */}
         <div className="card card-no-hover w-full mb-6 relative overflow-visible flex-shrink-0 group bg-white border border-blue-100">
-          <div className="relative z-10 flex items-center gap-6 p-6">
+          <div className="relative z-10 flex items-center gap-6 px-6 py-1 pr-32">
             <div className="min-w-[120px]">
-              <h1 className="text-2xl font-bold text-[#2864D2] leading-tight">
-                Thi thử <br /> ĐGNL
+              <h1 className="text-3xl font-bold text-[#2864D2] leading-tight">
+                Thi thử <br /> V-ACT
               </h1>
             </div>
             <p className="text-slate-500 text-sm max-w-xl">
-              Lorem ipsum dolor sit amet consectetur. Vel vitae tellus vestibulum nunc.
-              Volutpat lacus sed ac aliquam adipiscing. Sagittis commodo massa in
-              ullamcorper congue velit. Vulputate bibendum quis interdum nec purus id.
+              Bản lĩnh không tự nhiên sinh ra, nó phải được tôi luyện.
+              Hãy để tiếng đồng hồ đếm ngược rèn cho tim bạn nhịp đập thép, tập làm quen với cảm giác căng thẳng ngay tại nhà. 
+              Chỉ khi bạn đổ mồ hôi trên 'sân tập' với cường độ của một trận đấu thật, 
+              thì phòng thi chính thức mới không còn là nỗi ám ảnh, mà trở thành nơi để bạn tỏa sáng.
             </p>
           </div>
 
-          {/* Figures (Giữ tone vàng như mẫu) */}
-          <div className="absolute right-2 top-[-14px] bottom-[-14px] w-64 pointer-events-none flex items-center justify-center">
-            <img
-              src="/assets/icons/exam_icon.svg"
-              className="w-56 h-auto overflow-visible"
-            />
+          {/* Figures*/}
+          <div className="absolute inset-0 pointer-events-none">
+            <div className="absolute right-6 top-[-32px] bottom-0 w-73 overflow-hidden flex items-start justify-center">
+              <img
+                src="/assets/icons/exam_icon.svg"
+                className="w-63 h-auto"
+                alt="Exam icon"
+              />
+            </div>
           </div>
         </div>
 
